ab3297f8bbcdcdb8c69b3584ae1b8a78868ec68f
kaRa4065 <msravirani@gmail.com>
2026-02-15 21:19:03 +0530

    fix

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -28,21 +28,11 @@ const useChat = () => {
     setLoading(true);
 
     try {
-      const res = await fetch(
-        `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            contents: [
-              {
-                role: "user",
-                parts: [{ text }],
-              },
-            ],
-          }),
-        },
-      );
+      const res = await fetch(`https://karthickbio.vercel.app/api/gemini`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ prompt }),
+      });
 
       if (!res.ok) throw new Error("API Error");
 
